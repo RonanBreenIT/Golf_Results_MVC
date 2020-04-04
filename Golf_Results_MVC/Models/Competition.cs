133d2bbaa3fff320a8607c4ac92adfecd573bb65
Ronan Breen <X00152190@mytudublin.ie>
2020-04-04 11:39:54 +0100

    Changed Season in Comp_Result to Int as only need the Year. Changed Date formats for Comp. Changed Results to display only Dates instead of start and End date.

--- Golf_Results_MVC/Models/Competition.cs
+++ Golf_Results_MVC/Models/Competition.cs
@@ -13,14 +13,24 @@ namespace Golf_Results_MVC.Models
         public string Name { get; set; }
 
         [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM}", ApplyFormatInEditMode = true)]
         [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
 
         [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM}", ApplyFormatInEditMode = true)]
         [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
+
+        [Display(Name = "Dates")]
+        //[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public string FullDate
+        {
+            get
+            {
+                return StartDate.ToShortDateString() + " - " + EndDate.ToShortDateString();
+            }
+        }
 
         public virtual ICollection<Comp_Result> Comp_Result { get; set; }
 
