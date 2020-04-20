b468de9bd3a5f839b437b1e52880ca7702237cac
Ronan Breen <X00152190@mytudublin.ie>
2020-04-20 20:22:05 +0100

    Enabled createusersandroles in startup so automatically my emaial would be assigned admin.

--- Golf_Results_MVC/Models/Competition.cs
+++ Golf_Results_MVC/Models/Competition.cs
@@ -11,7 +11,6 @@ namespace Golf_Results_MVC.Models
         [Required]
         [Display(Name = "Name"), StringLength(50, MinimumLength = 1, ErrorMessage = "Competition name cannot be longer than 50 characters or null.")]
         public string Name { get; set; }
-
         public virtual ICollection<Comp_Result> Comp_Result { get; set; } // This needed to display all their results
     }
 }
